0aaf218b7fbe4324a71d5b4a0dba9841f79687d9
chenguibiao <chenguibiao@yy.com>
2021-04-20 18:39:17 +0800

    修改模板引擎

--- app.js
+++ app.js
@@ -6,24 +6,28 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
+app.locals.serverData = {
+  token: ''
+}
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/test', (req, res) => {
-  res.json({ username: 'Flavio' })
-});
+app.use('/api', apiRouter);
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.locals.serverData = {
+    hello: 'world'
+  }
+  res.render('index', { title: 'Express', serverData: {
+    hello2: 'world2'
+  } });
 });
 
 module.exports = router;
